07874d0754f394921181a2ea36530465e8ece00f
Samuel Ezedi <samuelezedi@gmail.com>
2020-07-09 16:56:45 +0100

    made app bar show title when scroll is active and not to show when expanded

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MainView()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: MainView());
   }
 }
 
@@ -27,187 +24,186 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
-
   final double appBarHeight = 66.0;
+
+  bool showHeaderText = false;
+
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _scrollController = ScrollController()
+      ..addListener(() {
+        print('here');
+        if (_isAppBarExpanded) {
+          setState(() {
+            showHeaderText = true;
+            print('setState is called with white');
+          });
+        } else {
+          if (showHeaderText == true) {
+            setState(() {
+              showHeaderText = false;
+              print('setState is called with white');
+            });
+          }
+        }
+      });
+  }
+
+  bool get _isAppBarExpanded {
+    return _scrollController.hasClients &&
+        _scrollController.offset > (200 - kToolbarHeight);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final double statusBarHeight = MediaQuery
-        .of(context)
-        .padding
-        .top;
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
 
     return Scaffold(
+      backgroundColor: Color(0xff013db7),
       body: CustomScrollView(
+        controller: _scrollController,
         slivers: <Widget>[
-
           SliverAppBar(
-            title: Text('All Notes'),
             pinned: true,
-
+            elevation: 0,
+            backgroundColor: Color(0xff013db7),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
             expandedHeight: 250,
-            onStretchTrigger: (){
-
+            onStretchTrigger: () {
+              return;
             },
             flexibleSpace: FlexibleSpaceBar(
+              title: showHeaderText ? Text('All Notes') : null,
+              centerTitle: true,
               background: Container(
                 padding: new EdgeInsets.only(top: statusBarHeight),
                 height: statusBarHeight + appBarHeight,
                 child: new Center(
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Container(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(child: new Text(
-                                  "Balance",
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontFamily: 'Poppins',
-                                      fontSize: 28.0
-                                  )
-                              ),),
-                              Container(child: new Text(
-                                  "\u002420,914.33",
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontFamily: 'Poppins',
-                                      fontWeight: FontWeight.w800,
-                                      fontSize: 36.0
-                                  )
-                              ),),
-                            ],),),
-
-                        Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(child: Padding(
-                                padding: const EdgeInsets.only(bottom: 16.0),
-                                child: new Text(
-                                    "\u002B24.93\u0025",
-                                    style: const TextStyle(
-                                        color: Colors.white70,
-                                        fontFamily: 'Poppins',
-                                        fontSize: 20.0
-                                    )
-                                ),
-                              ),),
-
-                              Container(child: Padding(
-                                padding: const EdgeInsets.only(bottom: 16.0),
-                                child: Icon(
-                                  Icons.arrow_upward, color: Colors.green,),
-                              ),),
-
-                            ],),
-                        ),
-
-
-                        Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Container(child: Padding(
-                                padding: const EdgeInsets.only(bottom: 8.0,left:8.0),
-                                child: new Text(
-                                    "Currency",
-                                    style: const TextStyle(
-                                        color: Colors.white70,
-                                        fontFamily: 'Poppins',
-                                        fontSize: 16.0
-                                    )
-                                ),
-                              ),),
-
-                              Container(child: Padding(
-                                padding: const EdgeInsets.only(bottom: 8.0,right:8.0),
-                                child: Container(
-                                    child: Row(children: <Widget>[
-                                      Container(child: Icon(
-                                        Icons.calendar_today, color: Colors.white,
-                                      ),),
-                                      SizedBox(width: 10,),
-                                      Container(child: Text(
-                                        'Janaury 2019', style: const TextStyle(
-                                          color: Colors.white70,
-                                          fontFamily: 'Poppins',
-                                          fontSize: 16.0
-                                      ),),),
-                                    ],)
-
-                                ),
-                              ),),
-
-
-                            ],),
-                        ),
-                      ],)
-                ),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            child: new Text("All Notes",
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Poppins',
+                                    fontSize: 28.0)),
+                          ),
+                          Container(
+                            child: new Text("note count",
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 15.0)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
                 decoration: new BoxDecoration(
                   color: Color(0xff013db7),
                 ),
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                  Padding(
-                    padding: const EdgeInsets.all(28.0),
-                    child: Text('123'),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
+          SliverGrid.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
-                ),Padding(
-                padding: const EdgeInsets.all(28.0),
-                child: Text('123'),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
               ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(28.0),
-                  child: Text('123'),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
                 ),
-
-
-
-
-              ]
-            )
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                ),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                ),
+                margin: EdgeInsets.all(10),
+                child: Text('dsf'),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
 }
-
