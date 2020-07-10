41e5a4d6a49cb2eddd17a3880e6bd5085083f9ba
Samuel Ezedi <samuelezedi@gmail.com>
2020-07-10 18:15:41 +0100

    advanced create.dart code

--- lib/screens/create.dart
+++ lib/screens/create.dart
@@ -1,4 +1,3 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
@@ -12,17 +11,21 @@ class CreateNote extends StatefulWidget {
   String type;
   Timestamp created;
   Timestamp updated;
-  CreateNote(this.user, this.type, {this.noteTitle, this.noteContent, this.created, this.updated});
+  CreateNote(this.user, this.type,
+      {this.noteTitle, this.noteContent, this.created, this.updated});
   @override
   _CreateNoteState createState() => _CreateNoteState();
 }
 
 class _CreateNoteState extends State<CreateNote> {
-
+  String prevNoteTitle;
+  String prevNoteContent;
   String noteTitle;
   String noteContent;
   String user;
   String type;
+  BuildContext mContext;
+  bool notSave = true;
 
   @override
   void initState() {
@@ -30,160 +33,184 @@ class _CreateNoteState extends State<CreateNote> {
     setState(() {
       noteContent = widget.noteContent;
       noteTitle = widget.noteTitle;
+      prevNoteContent = widget.noteContent;
+      prevNoteTitle = widget.noteTitle;
       user = widget.user;
       type = widget.type;
     });
   }
 
   saveNote() {
-    if(this.type == "1"){
-      //means new note
-      Notes notes = Notes(
-          title: noteTitle,
-          content: noteContent,
-          timestamp: Timestamp.now(),
-          updated: Timestamp.now(),
-          userId: this.user
-      );
-      Firestore.instance.collection('notes')
-          .add(notes.toMap()).then((value) {
-        Flash().show(context, 2, 'Saved', Colors.black54, 15, null,null);
-      });
-    } else {
-
-      Notes notes = Notes(
-        title: noteTitle,
-        content: noteContent,
-        updated: Timestamp.now(),
-        timestamp: widget.created,
-        userId: widget.user
-      );
-      //means old note
-      Firestore.instance.collection('notes')
-          .document(this.type)
-          .updateData(
-          notes.toMap()
-      ).then((value) {
-        Flash().show(context, 2, 'Saved', Colors.black54, 15, null,null);
-      });
+    if (notSave) {
+      if (prevNoteContent != noteContent || prevNoteTitle != noteTitle) {
+        if (this.type == "1") {
+          //means new note
+          Notes notes = Notes(
+              title: noteTitle,
+              content: noteContent,
+              timestamp: Timestamp.now(),
+              updated: Timestamp.now(),
+              userId: this.user);
+          Firestore.instance
+              .collection('notes')
+              .add(notes.toMap())
+              .then((value) {
+            setState(() {
+              notSave = false;
+            });
+            Flash().show(context, 2, 'Saved', Colors.black54, 15, null, null);
+          });
+        } else {
+          Notes notes = Notes(
+              title: noteTitle,
+              content: noteContent,
+              updated: Timestamp.now(),
+              timestamp: widget.created,
+              userId: widget.user);
+          //means old note
+          Firestore.instance
+              .collection('notes')
+              .document(this.type)
+              .updateData(notes.toMap())
+              .then((value) {
+            setState(() {
+              notSave = false;
+            });
+            Flash().show(context, 2, 'Saved', Colors.black54, 15, null, null);
+          });
+        }
+      } else {
+        Flash().show(mContext, 1, 'You have nothing to save', Colors.red, 15,
+            Colors.white, 2);
+      }
     }
   }
 
   autoSaveNote() {
-    if(this.type == "1"){
-      //means new note
-      Notes notes = Notes(
-        title: noteTitle,
-        content: noteContent,
-        timestamp: Timestamp.now(),
-        updated: Timestamp.now(),
-        userId: this.user
-      );
-      Firestore.instance.collection('notes')
-      .add(notes.toMap()).then((value) {
-        Flash().show(context, 2, 'Saved', Colors.black54, 15, null,null);
-      });
-    } else {
-
-      Notes notes = Notes(
-        title: noteTitle,
-        content: noteContent,
-        updated: Timestamp.now(),
-          timestamp: widget.created,
-          userId: widget.user
-      );
-      //means old note
-      Firestore.instance.collection('notes')
-      .document(this.type)
-          .updateData(
-          notes.toMap()
-      ).then((value) {
-        Flash().show(context, 2, 'Saved', Colors.black54, 15, null,null);
-      });
+    if (notSave) {
+      if (prevNoteContent != noteContent || prevNoteTitle != noteTitle) {
+        if (this.type == "1") {
+          //means new note
+          Notes notes = Notes(
+              title: noteTitle,
+              content: noteContent,
+              timestamp: Timestamp.now(),
+              updated: Timestamp.now(),
+              userId: this.user);
+          Firestore.instance
+              .collection('notes')
+              .add(notes.toMap())
+              .then((value) {
+            Flash().show(context, 2, 'Saved', Colors.black54, 15, null, null);
+          });
+        } else {
+          Notes notes = Notes(
+              title: noteTitle,
+              content: noteContent,
+              updated: Timestamp.now(),
+              timestamp: widget.created,
+              userId: widget.user);
+          //means old note
+          Firestore.instance
+              .collection('notes')
+              .document(this.type)
+              .updateData(notes.toMap())
+              .then((value) {
+            Flash().show(context, 2, 'Saved', Colors.black54, 15, null, null);
+          });
+        }
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: IconButton(
-          onPressed: (){
-            autoSaveNote();
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_ios,color: Colors.black54,),
-        ),
-        actions: <Widget>[
-          InkWell(
-            child: Padding(
-              padding: EdgeInsets.only(top: 13, bottom: 13, right: 30),
-              child: Text('Save',style: TextStyle(color: Colors.black54,fontSize: 20),),
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            onPressed: () {
+              autoSaveNote();
+              Navigator.pop(context);
+            },
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black54,
             ),
           ),
-
-        ],
-      ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                onChanged: (input){
-                  setState(() {
-                    noteTitle = input;
-                  });
-                },
-                initialValue: noteTitle,
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400
-                ),
-                decoration: InputDecoration(
-                  hintText: 'Enter Title',
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide.none
-                  ),
+          actions: <Widget>[
+            InkWell(
+              onTap: () {
+                saveNote();
+              },
+              child: Padding(
+                padding: EdgeInsets.only(top: 13, bottom: 13, right: 30),
+                child: Text(
+                  'Save',
+                  style: TextStyle(color: Colors.black54, fontSize: 20),
                 ),
               ),
-              Wrap(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('Notes:',style: TextStyle(fontSize: 17,
-                    ),),
+            ),
+          ],
+        ),
+        body: Builder(builder: (context) {
+          mContext = context;
+          return Container(
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  onChanged: (input) {
+                    setState(() {
+                      noteTitle = input;
+                      notSave = false;
+                    });
+                  },
+                  initialValue: noteTitle,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                  decoration: InputDecoration(
+                    hintText: 'Enter Title',
+                    enabledBorder:
+                        OutlineInputBorder(borderSide: BorderSide.none),
+                    focusedBorder:
+                        OutlineInputBorder(borderSide: BorderSide.none),
                   ),
-                  TextFormField(
-                    onChanged: (input){
-                      setState(() {
-                        noteContent = input;
-                      });
-                    },
-                    initialValue: noteContent,
-                    minLines: 1,
-                    maxLines: 300,
-                    style: TextStyle(
-                        fontSize: 17
-                    ),
-                    decoration: InputDecoration(
-                      hintText: 'Enter notes',
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide.none
+                ),
+                Wrap(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Notes:',
+                        style: TextStyle(
+                          fontSize: 17,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        )
-    );
+                    TextFormField(
+                      onChanged: (input) {
+                        setState(() {
+                          noteContent = input;
+                          notSave = false;
+                        });
+                      },
+                      initialValue: noteContent,
+                      minLines: 1,
+                      maxLines: 300,
+                      style: TextStyle(fontSize: 17),
+                      decoration: InputDecoration(
+                        hintText: 'Enter notes',
+                        enabledBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none),
+                        focusedBorder:
+                            OutlineInputBorder(borderSide: BorderSide.none),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        }));
   }
 }
