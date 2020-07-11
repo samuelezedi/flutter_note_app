0e39bd89f76d50f43839c3ee89a745324b53430b
Samuel Ezedi <samuelezedi@gmail.com>
2020-07-11 17:28:44 +0100

    added launcher class into main.dart, to get user id

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:notably/screens/all_notes.dart';
-import 'package:notably/screens/selected.dart';
 import 'package:notably/utils/theme.dart';
-import 'package:notably/widgets/drawer.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:device_info/device_info.dart';
+import 'dart:io';
+
+//device varibles
+String deviceName; //pname
+String deviceId; //puid
+String deviceBoard; //pboard
+String deviceModelId; //pmodel
+
+String currentUser;
 
 void main() {
   runApp(MyApp());
@@ -21,15 +31,143 @@ class MyApp extends StatelessWidget {
           primarySwatch: MaterialColor(AppTheme.colorCode1, AppTheme.colorCodes),
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: AppBody());
+        home: Launcher());
   }
 }
 
-class AppBody extends StatelessWidget {
+
+class Launcher extends StatefulWidget {
+  @override
+  _LauncherState createState() => _LauncherState();
+}
+
+class _LauncherState extends State<Launcher> {
+  FirebaseMessaging _messaging = FirebaseMessaging();
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+  String user;
+
+  @override
+  void initState() {
+    super.initState();
+    getDeviceDetails().then((value) {
+
+    });
+  }
+
+
+  Future getDeviceDetails() async {
+    SharedPreferences local = await SharedPreferences.getInstance();
+    _messaging.getToken().then((token) async {
+      if (Platform.isIOS) {
+        iosDevice().then((uid) {
+          setState(() {
+            this.user = "$deviceId-$deviceName";
+          });
+          Firestore.instance
+              .collection('users')
+              .where('puid', isEqualTo: deviceId)
+              .where('pname', isEqualTo: deviceName)
+              .where('pboard', isEqualTo: deviceBoard)
+              .where('pmodel', isEqualTo: deviceModelId)
+              .getDocuments()
+              .then((checking) {
+            if (checking.documents.length > 0) {
+              //update the token always
+
+
+              Firestore.instance
+                  .collection('users')
+                  .document(checking.documents[0].documentID)
+                  .updateData({
+                'ptoken': token.toString(),
+              });
+            } else {
+              Firestore.instance.collection('users').add({
+                'puid': deviceId,
+                'pname': deviceName,
+                'pboard': deviceBoard,
+                'pmodel': deviceModelId,
+                'ptoken': token.toString(),
+                'time_joined': Timestamp.now(),
+                'email': '',
+                'id' : "$deviceId-$deviceName"
+              });
+            }
+          });
+        });
+      } else {
+        androidDevice().then((uid) {
+          setState(() {
+            this.user = "$deviceId-$deviceName";
+          });
+          Firestore.instance
+              .collection('users')
+              .where('puid', isEqualTo: deviceId)
+              .where('pname', isEqualTo: deviceName)
+              .where('pboard', isEqualTo: deviceBoard)
+              .where('pmodel', isEqualTo: deviceModelId)
+              .getDocuments()
+              .then((checking) {
+
+            if (checking.documents.length > 0) {
+              //check if blocked
+
+
+              Firestore.instance
+                  .collection('users')
+                  .document(checking.documents[0].documentID)
+                  .updateData({
+                'ptoken': token.toString(),
+              });
+            } else {
+
+              Firestore.instance.collection('users').add({
+                'puid': deviceId,
+                'pname': deviceName,
+                'pboard': deviceBoard,
+                'pmodel': deviceModelId,
+                'ptoken': token.toString(),
+                'time_joined': Timestamp.now(),
+                'email': '',
+                'id' : "$deviceId-$deviceName"
+              });
+            }
+          });
+        });
+      }
+    });
+    //save uid in cloud and messaging token
+  }
+
+  Future<bool> androidDevice() async {
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    deviceName = androidInfo.model;
+    deviceId = androidInfo.androidId; //UUID for Android
+    deviceModelId = androidInfo.id;
+    deviceBoard = androidInfo.board;
+    currentUser = "$deviceId-$deviceName";
+    return true;
+  }
+
+  Future<bool> iosDevice() async {
+    IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+    deviceName = iosInfo.name;
+    deviceBoard = iosInfo.systemVersion;
+    deviceId = iosInfo.identifierForVendor;
+    deviceModelId = iosInfo.localizedModel;
+    currentUser = "$deviceId-$deviceName";
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return NotesView('sdf', pageTitle: 'All Notes',selector: false,selectedNotes: [],allSnapshot: <String>[],);
+    return NotesView(this.user, pageTitle: 'All Notes',selector: false,selectedNotes: [],allSnapshot: <String>[]);
   }
 }
+
+
+
+
 
 
